ad6f31ced2ae624667cf39f9154588346dcd144b
Almaz <almazzz-2@yandex.ru>
2024-02-25 15:42:38 +0300

    fix

--- backend/models_genearateor/main.py
+++ backend/models_genearateor/main.py
@@ -136,7 +136,7 @@ class layer_models:
                         if (i > y):
                             model[i][j] = self.layerValues[o + 1]
         else:
-            if self.sole:
+            if len(self.sole) != 0:
                 prevSole = []
                 for o in self.sole:
                     prevSole.append(random.randint(o[0], o[1]))
